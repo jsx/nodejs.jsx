b6cade4e97751a8059a3a8f695c573e21fac316b
Kazuho Oku <kazuhooku@gmail.com>
2013-09-02 16:49:44 +0900

    add crypto (hash, random only)

---
+++ lib/nodejs/crypto.jsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) DeNA Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import "nodejs.jsx";
+
+native __fake__ class crypto {
+
+	static function getCiphers() : string[];
+
+	static function getHashes() : string[];
+
+	static function createHash(algorithm : string) : Hash;
+
+	static function randomBytes(size : number) : Buffer;
+	static function randomBytes(size : number, callback : function (ex : variant, buf : Buffer) : void) : void;
+
+	static function pseudoRandomBytes(size : number) : Buffer;
+	static function pseudoRandomBytes(size : number, callback : function (ex : variant, buf : Buffer) : void) : void;
+
+} = "require('crypto')";
+
+/* FIXME should better be a fake interface */
+native __fake__ class Hash {
+	function update(data : variant) : void;
+	function update(data : variant, input_encoding : string) : void;
+	function digest() : Buffer;
+	function digest(encoding : string) : variant;
+}

324196efb012096af94b89572c0e59a5456cf3c2
Kazuho Oku <kazuhooku@gmail.com>
2013-09-18 09:20:05 +0900

    add test for POST method using the http module

--- test/http.jsx
+++ test/http.jsx
@@ -21,4 +21,31 @@ class _Test extends TestCase {
 		}, 10000);
 	}
 
+	function testPost() : void {
+		this.async((async) -> {
+			var req = http.request({
+				method: "POST",
+				hostname: "www.google.com",
+				port: 80,
+				path: "/search",
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}
+			}, function (res) {
+				this.expect(res.statusCode).toBe(405);
+				res.setEncoding("utf8");
+				var data = "";
+				res.on("data", function (chunk) {
+					data += chunk as string;
+				});
+				res.on("end", function () {
+					log data;
+					async.done();
+				});
+			});
+			req.write("q=test");
+			req.end();
+		}, 10000);
+	}
+
 }

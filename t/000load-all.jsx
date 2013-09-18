8fbd754b7f503f82fbdd284133cebc74d3b389d2
Kazuho Oku <kazuhooku@gmail.com>
2013-09-18 13:57:33 +0900

    reorganize the tests so that they can be run by: prove t/*.jsx

---
+++ t/000load-all.jsx
@@ -0,0 +1,5 @@
+import "test-case.jsx";
+import "nodejs/*.jsx";
+
+class _Test extends TestCase {
+}
--- test/load-all.jsx
+++
@@ -1,6 +0,0 @@
-import "nodejs/*.jsx";
-
-class _Main {
-    static function main(args : string[]) : void {
-    }
-}
